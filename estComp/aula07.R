f7f149feb2175e550fbaa53111ac009e951fed1e
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2022-09-23 22:03:21 -0300

    aula de r uma bosta

---
+++ estComp/aula07.R
@@ -0,0 +1,43 @@
+
+
+
+
+#1
+A <- matrix(c(2,5,2,6,1,4),2,3)
+
+a <- A*3
+
+#2
+lista <- scan()
+D <- matrix(lista,3,3)
+
+
+#3
+
+matriz <- matrix(c(1,2,2,4,7,6),3,2,1)
+matriz2 <- matrix(c(10,20,30,40,50,60),3,2,1)
+
+x <- matriz - matriz2
+B <- (2/7) * x
+
+#4
+B%*%A
+
+#5
+range(A)
+#Valor mais alto e mais baixo
+
+#6
+sum(A)
+
+#7
+prod(A)
+
+#8
+colSums(A)
+
+#9
+sum(B[B<4])
+
+#10
+
