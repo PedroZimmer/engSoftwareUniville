1f3585a1eb9fc3835f680b5e6b1ad0f7e1729422
PEDRO ECCEL ZIMMERMANN <pedro.zimmermann@univille.br>
2022-09-29 20:27:45 -0300

    ex02

---
+++ alpc_II/2oBimestre/Atividades_1/ex02.py
@@ -0,0 +1,9 @@
+
+'''
+2)	Faça uma função que transforme e mostre segundos em horas, minutos e segundos. Todas as variáveis devem ser passadas como parâmetro, não havendo variáveis globais.
+a.	1h = 3600s
+b.	1m = 60s
+
+'''
+
+
