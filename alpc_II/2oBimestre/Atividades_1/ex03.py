a275231f542755cd4e9e05510874f48c6ed33413
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2022-09-30 08:06:40 -0300

    ex03

---
+++ alpc_II/2oBimestre/Atividades_1/ex03.py
@@ -0,0 +1,7 @@
+'''
+1)	Faça uma função que receba as três notas de um aluno como parâmetro e uma letra.
+Se a letra for A o procedimento calcula a média aritmética das notas do aluno, 
+se for P o procedimento calcula a média ponderada com os pesos 5,3 e 2.
+A média calculada deve ser devolvida ao programa principal para, então, ser mostrada.
+
+'''
