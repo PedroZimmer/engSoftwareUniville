47439c75970c8c2e13f53d3e0fc180a1e9d70a9f
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2022-09-30 08:49:00 -0300

    ex05

---
+++ alpc_II/2oBimestre/Atividades_1/ex05.py
@@ -0,0 +1,15 @@
+'''
+5)	Foi realizada uma pesquisa de algumas características físicas de cinco habitantes de uma certa região. 
+De cada habitante foram coletados os seguintes dados: sexo, cor dos olhos (A – Azuis ou C – Castanhos),
+cor dos cabelos (L – Louros, P – Pretos ou C – Castanhos) e idade.
+
+a.	Faça uma função que leia esses dados em vetores. Determine, por meio de outra função,
+    a média de idade das pessoas com olhos castanhos e cabelos pretos. Mostre esse resultado no programa principal.
+    
+b.	Faça uma função que determine e devolva ao programa principal a maior idade entre os habitantes.
+
+c.	Faça uma função que determine e devolva ao programa principal a quantidade de indivíduos do sexo feminino
+    cuja idade está entre 18 e 35 (inclusive) e que tenham olhos azuis e cabelos louros.
+'''
+
+
