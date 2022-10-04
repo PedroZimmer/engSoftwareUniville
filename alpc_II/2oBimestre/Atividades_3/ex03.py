8b231ffa5bab3c63c2fb2cc0194d1cc5979ec1ab
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2022-10-03 23:06:20 -0300

    atv3ex03

---
+++ alpc_II/2oBimestre/Atividades_3/ex03.py
@@ -0,0 +1,9 @@
+'''
+3.	Elabore uma função que retorne um vetor com os três primeiros números perfeitos.
+Sabe-se que um número é perfeito quando é igual à soma de seus divisores (exceto ele mesmo).
+Exemplo: os divisores de 6 são: 1 , 2 e 3, e 1+2+3 = 6, logo 6 é perfeito.
+'''
+
+def funcao():
+    for i in range(1, 100):
+        
