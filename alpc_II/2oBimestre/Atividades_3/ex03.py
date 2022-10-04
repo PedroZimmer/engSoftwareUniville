200a369677cde8acd750f33dd5ce08d429dbd107
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2022-10-04 09:15:48 -0300

    atv3 ex3 feito racha cuca kk

--- alpc_II/2oBimestre/Atividades_3/ex03.py
+++ alpc_II/2oBimestre/Atividades_3/ex03.py
@@ -5,5 +5,14 @@ Exemplo: os divisores de 6 são: 1 , 2 e 3, e 1+2+3 = 6, logo 6 é perfeito.
 '''
 
 def funcao():
-    for i in range(1, 100):
-        
+    perfeitos = []
+    for i in range(1, 1000):
+        lista = []
+        for j in range(1,1000):
+            if i > j:
+                if i % j == 0:
+                    lista.append(j)
+        if sum(lista) == i:
+            perfeitos.append(i)
+    return perfeitos
+print(funcao())
