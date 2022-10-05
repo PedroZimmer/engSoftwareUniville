c1a1c4424e8fe410189218897f485d221a05c3e5
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2022-10-04 22:07:16 -0300

    aula dia 04-10-2022

---
+++ alpc_II/2oBimestre/Atividades_3/ex04.py
@@ -0,0 +1,17 @@
+'''
+4.	Crie uma função que receba como parâmetro um valor inteiro e positivo N e 
+retorne o valor de S que é obtido pelo seguinte cálculo:
+S = 1 + 1/1! + 1/2! + 1/3! + ... + 1/N!
+
+'''
+
+
+def funcao(num):
+    for i in range(1,num):
+        fatorial = i
+        soma += 1/i
+        
+        
+
+
+num = int(input("Numero: "))
