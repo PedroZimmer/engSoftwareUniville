2f6d98db3ae1942619f196b251dedc22329e0733
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2022-10-06 19:34:17 -0300

    atv3 ex5 pronto

--- alpc_II/2oBimestre/Atividades_3/ex05.py
+++ alpc_II/2oBimestre/Atividades_3/ex05.py
@@ -11,3 +11,28 @@ def funcao(hr_ini, min_ini, hr_fim, min_fim):
         diferença = (24 - hr_ini) + hr_fim
     else:
         diferença = hr_fim - hr_ini
+        
+        
+    if min_fim < min_ini:
+        diferençamin = (60 - min_ini) + min_fim
+        diferença -= 1
+    else:
+        diferençamin = min_fim - min_ini
+        
+    resultado = (diferença*60) + diferençamin
+    if resultado >= 1440:
+        resultado = "O jogo chegou em 24hrs... Insira os dados novamente!"
+        return resultado
+    else:
+        return resultado
+
+
+hr_ini = int(input("Digite a hora de inicio do jogo: "))
+min_ini = int(input("Digite os minutos de inicio do jogo: "))
+hr_fim = int(input("Digite a hora de fim do jogo: "))
+min_fim = int(input("Digite os minutos de fim do jogo: "))
+
+
+resposta = (funcao(hr_ini, min_ini, hr_fim, min_fim))
+print("Serão necessários {} minutos para o jogo terminar!".format(resposta))
+
