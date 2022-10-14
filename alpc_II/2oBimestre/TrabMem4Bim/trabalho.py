9bd93605928c49943fc9886180afb443f2366eeb
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2022-10-14 13:54:37 -0300

    ta foda pra chamar a função

--- alpc_II/2oBimestre/TrabMem4Bim/trabalho.py
+++ alpc_II/2oBimestre/TrabMem4Bim/trabalho.py
@@ -1,5 +1,46 @@
 import random
+from tracemalloc import start
 
+#PRIMEIRA ESCOLHA
+def primeiraescolha(tamanho, letra, memoria):
+    cont = 0
+    listavazio = []
+    while memoria:
+        if cont != tamanho:
+            for i in range(len(memoria)):
+                verificar = memoria[i]
+                if verificar == ' ':
+                    cont+=1
+                    listavazio.append(i)
+                    if tamanho > 1:
+                        for j in range(1, tamanho):
+                            if memoria[i+j-1] == ' ':
+                                cont+=1
+                                listavazio.append(i+1)
+                    else:
+                        memoria[i] = letra
+        else:
+            for i in listavazio:
+                memoria[i] = letra
+            pass
+    return memoria
+
+
+#ENTRADA DE DADOS
+def entradas():
+    tamanho = int(input("Tamanho: "))
+    letra = input("Letra: ")
+    return tamanho, letra
+
+
+#IMPRIMIR MEMORIA
+def imprimememoria():
+    for i in range(100):
+        if i % 20 == 0:
+            print("\n")
+        print(memoria[i], end='|')
+
+#INICIALIZAR MEMORIA
 memoria = [' '] * 100
 opcao = 0
 tamanho = 0
@@ -9,31 +50,34 @@ for i in range(100):
         memoria[i] = 'x'
     else:
         memoria[i] = ' '
-#Aqui você deve imprimir todo o conteúdo da variável memória
 
 
-while(opcao != 4):
-    #Menu do programa
-    print("1 - Primeira Escolha")
-    print("2 - Melhor Escolha")
-    print("3 - Pior Escolha")
-    print("4 - Sair")
-    print("Escolha o algoritmo pelo numero")
-    opcao = int(input())
-    print("Digite o tamanho da informacao")
-    tamanho = int(input())
-    print("Digite a letra a ser utiliada")
-    letra = input()
-
-    if(opcao == 1):
-        #Aqui você deve implementar o algoritmo de primeira escolha
-        pass
-    else:
-        if (opcao == 2):
-            #Implemente aqui a lógica da melhor escolha
+#MENU
+while(opcao != 5):
+    print("\n1 - Primeira Escolha\n2 - Melhor Escolha\n3 - Pior Escolha\n4 - Imprimir Memória\n5 - Sair")
+    opcao = int(input(("\nOq deseja fazer? "))) #OPÇÃO DO MENU
+      
+    #IMPRIMIR A MEMÓRIA  
+    if(opcao == 4):
+        imprimememoria()
+    if opcao != 4 and opcao != 5:
+        #PRIMEIRA ESCOLA
+        if(opcao == 1):
+            tamanho,letra = entradas() #ENTRADA DE DADOS
+            resprimeiarescolha = primeiraescolha(tamanho, letra, memoria) #CHAMADA DA FUNÇÃO
+            print(resprimeiarescolha)
             pass
         else:
-            if(opcao == 3):
-                #Implemente aqui a lógica da pior escolha
+            #MELHOR ESCOLHA
+            if (opcao == 2):
                 pass
-    # Aqui você deve imprimir todo o conteúdo da variável memória
+            else:
+                if(opcao == 3):
+                    pass
+                else:
+                    if(opcao == 5):
+                        print("Saindo...")
+                        pass
+                    else:
+                        print("Opcao invalida")
+                        pass
