f3f63e786cc2b4cf5d7de2104990fd5e563d3879
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2022-10-20 15:21:50 -0300

    sla oq ta modificado

--- alpc_II/2oBimestre/TrabMem4Bim/trabalho.py
+++ alpc_II/2oBimestre/TrabMem4Bim/trabalho.py
@@ -1,7 +1,8 @@
 import random
 from tracemalloc import start
 
-#PRIMEIRA ESCOLHA
+# PRIMEIRA ESCOLHA
+'''
 def primeiraescolha(tamanho, letra, memoria):
     print(tamanho, letra)
     cont = 0
@@ -29,52 +30,84 @@ def primeiraescolha(tamanho, letra, memoria):
                 terminou = True
             pass
     return memoria
+'''
 
 
-#ENTRADA DE DADOS
+def primeiraescolha(tamanho, letra, memoria):
+    listadosvazios = []
+    vazioscolados = []
+    for i in range(100):
+        if memoria[i] == ' ':
+            listadosvazios.append(i)
+    if tamanho == 1:
+        memoria[listadosvazios[0]] = letra
+        return memoria
+    else:
+        achou = False
+        for j in range(len(listadosvazios)):
+            if j == listadosvazios[-1]:
+                break
+            if listadosvazios[j] + 1 == listadosvazios[j+1]:
+                if achou == False:
+                    vazioscolados.append(listadosvazios[j])
+                vazioscolados.append(listadosvazios[j+1])
+                achou = True
+            else:
+                if len(vazioscolados) == 0:
+                    achou = False
+                    if len(vazioscolados) > tamanho:
+                        vazioscolados = []
+            if len(vazioscolados) == tamanho:
+                for k in vazioscolados:
+                    memoria[k] = letra
+                return memoria
+
+
+# ENTRADA DE DADOS
 def entradas():
     tamanho = int(input("Tamanho: "))
     letra = input("Letra: ")
     return tamanho, letra
 
 
-#IMPRIMIR MEMORIA
+# IMPRIMIR MEMORIA
 def imprimememoria():
     for i in range(100):
-        if i % 20 == 0: #QUEBRA A CADA 20 POSIÇÕES
+        if i % 20 == 0:  # QUEBRA A CADA 20 POSIÇÕES
             print("\n")
         print(memoria[i], end='|')
 
-#INICIALIZAR MEMORIA
+
+# INICIALIZAR MEMORIA
 memoria = [' '] * 100
 opcao = 0
 tamanho = 0
 letra = ''
 for i in range(100):
-    if(random.randint(0,11) >= 5):
+    if(random.randint(0, 11) >= 5):
         memoria[i] = 'x'
     else:
         memoria[i] = ' '
 
 
-#MENU
+# MENU
 while(opcao != 5):
     print("\n1 - Primeira Escolha\n2 - Melhor Escolha\n3 - Pior Escolha\n4 - Imprimir Memória\n5 - Sair")
-    opcao = int(input(("\nOq deseja fazer? "))) #OPÇÃO DO MENU
-      
-    #IMPRIMIR A MEMÓRIA  
+    opcao = int(input(("\nOq deseja fazer? ")))  # OPÇÃO DO MENU
+
+    # IMPRIMIR A MEMÓRIA
     if(opcao == 4):
         imprimememoria()
     if opcao != 4 and opcao != 5:
-        #PRIMEIRA ESCOLA
+        # PRIMEIRA ESCOLA
         if(opcao == 1):
-            tamanho,letra = entradas() #ENTRADA DE DADOS
+            tamanho, letra = entradas()  # ENTRADA DE DADOS
             print("Memória antes da alocação: ", memoria)
-            primeiraescolha(tamanho, letra, memoria) #CHAMADA DA FUNÇÃO
-            #print(resprimeiarescolha)
+            primeiraescolha(tamanho, letra, memoria)  # CHAMADA DA FUNÇÃO
+            # print(resprimeiarescolha)
             pass
         else:
-            #MELHOR ESCOLHA
+            # MELHOR ESCOLHA
             if (opcao == 2):
                 pass
             else:
