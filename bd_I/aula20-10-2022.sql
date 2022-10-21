ab7c9eb52830016f04184de0e8253e93a37a4b4c
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2022-10-20 21:41:59 -0300

    aula 20/10/2022

---
+++ bd_I/aula20-10-2022.sql
@@ -0,0 +1,19 @@
+SELECT ClienteNome, ContaTipo,
+contas.ClienteCodigo, clientes.ClienteCodigo
+FROM Clientes, Contas 
+WHERE Clientes.ClienteCodigo=Contas.ClienteCodigo
+
+
+SELECT ClienteNome, ContaTipo,
+contas.ClienteCodigo, clientes.ClienteCodigo
+FROM Clientes
+    INNER JOIN Contas ON (Clientes.ClienteCodigo=Contas.ClienteCodigo)
+    LEFT JOIN Movimentos ON (Contas.ContaNumero=Movimentos.ContaNumero)
+
+
+SELECT Clientes.ClienteNome, CartaoCredito.CartaoCodigo,
+CASE WHEN CartaoCodigo IS NULL THEN
+ClienteTelefone ELSE 'NÃO LIGAR' END AS 'SITUACAO'
+FROM Clientes
+LEFT JOIN CartaoCredito
+    ON (Clientes.ClienteCodigo=CartaoCredito.ClienteCodigo)
