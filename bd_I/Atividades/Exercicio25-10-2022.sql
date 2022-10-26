e894fa398d967894c1bb10acd084acf121cdb841
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2022-10-26 16:48:26 -0300

    aula bd 25-10-2022

--- bd_I/Atividades/Exercicio25-10-2022.sql
+++ bd_I/Atividades/Exercicio25-10-2022.sql
@@ -28,3 +28,5 @@ SELECT ClienteNome, AgenciaCodigo, ContaNumero, clientes.ClienteCodigo, contas.C
 
 INSERT INTO CartaoCredito (AgenciaCodigo, ContaNumero, ClienteCodigo, CartaoCodigo, CartaoLimite, CartaoExpira,CartaoCodigoSeguranca)
 VALUES (1, '173475-0', 1, '1234-7894-4561-1325', 10000, '2022-31-12', '123');
+
+-- 3
