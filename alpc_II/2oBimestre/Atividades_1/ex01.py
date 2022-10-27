80ffd0f7b17e5e9a1a2da7e7bddf4ea4e680bb6b
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2022-10-27 14:56:45 -0300

    ex01 refeito

--- alpc_II/2oBimestre/Atividades_1/ex01.py
+++ alpc_II/2oBimestre/Atividades_1/ex01.py
@@ -2,13 +2,19 @@
 1)	Faça uma função que receba três números inteiros:
  a,b e c, onde a é maior que 1. A função deve somar todos os inteiros entre b e c que sejam divisíveis por a (inclusive b e c) e retornar o resultado para a função principal.
 '''
-def funcao(a:int, b:int, c:int):
+def funcao(valores):
     soma = 0
+    a = valores[0]
+    b = valores[1]
+    c = valores[2]        
     for i in range(b,c):
         if i % a == 0:
             soma += i
     return soma
-va = int(input("Digite o valor de A:"))
-vb = int(input("Digite o valor de B:"))
-vc = int(input("Digite o valor de C:"))
-print(funcao(va,vb,vc))
+def inputs():
+    a = int(input("Insira o valor de A: "))
+    b = int(input("Insira o valor de B: "))
+    c = int(input("Insira o valor de C: "))
+    return [a,b,c]
+resultado = (funcao(inputs()))
+print("A soma dos valores é: ",resultado)
