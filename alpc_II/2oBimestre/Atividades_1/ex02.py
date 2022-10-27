875cb783c30c32b3d06e6510546e7df615bd72e6
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2022-10-27 15:11:54 -0300

    ex02 refeito

--- alpc_II/2oBimestre/Atividades_1/ex02.py
+++ alpc_II/2oBimestre/Atividades_1/ex02.py
@@ -1,16 +1,26 @@
 
 '''
-2)	Faça uma função que transforme e mostre segundos em horas, minutos e segundos. Todas as variáveis devem ser passadas como parâmetro, não havendo variáveis globais.
+2)	Faça uma função que transforme e mostre segundos em horas, minutos e segundos.
+Todas as variáveis devem ser passadas como parâmetro, não havendo variáveis globais.
 a.	1h = 3600s
 b.	1m = 60s
 
 '''
 
 def funcao(valor:int):
-    segemhoras = valor / 3600
-    segemmin = valor / 60
-    segemseg = valor / 1
-    return(segemhoras,segemmin,segemseg)
+    horas = seg_emhoras(valor)
+    minutos = seg_emmin(valor)
+    segundos = seg_emseg(valor)
+    return [horas,minutos,segundos]
+    
+def seg_emhoras(valor:int):
+    return valor / 3600
+
+def seg_emmin(valor:int):
+    return valor / 60
+
+def seg_emseg(valor:int):
+    return valor / 1
 
 valor = int(input("Insira os segundos..."))
 segemhoras,segemmin,segemseg = funcao(valor)
