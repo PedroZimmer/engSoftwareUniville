d9ba6a7b5b801a4e47adc8fc8735b4a9587c236c
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2022-11-11 09:50:17 -0300

    aula

---
+++ bd_I/aula11-10-2022.sql
@@ -0,0 +1,31 @@
+USE MinhaCaixa
+
+SELECT ClienteNome + ' ' + ClienteSobrenome
+ + '/' + CAST(ClienteRendaAnual AS CHAR(10)) AS NomeCompleto FROM Clientes
+
+
+
+ SELECT ClienteNome + ' ' + ClienteSobrenome
+ + '/' + CONVERT(CHAR(10), ClienteRendaAnual) AS NomeCompleto
+  FROM Clientes
+
+
+
+  SELECT ClienteNascimento,
+  DATENAME(MONTH,ClienteNascimento) AS MES,
+  YEAR(ClienteNascimento) as ANO,
+  DATENAME(MONTH,ClienteNascimento) + '/'+
+  CAST(YEAR(ClienteNascimento) AS CHAR(4)) as MESANO
+  FROM Clientes
+
+
+
+SELECT YEAR(GETDATE())
+
+DECLARE @ANO INT
+SET @ANO = (SELECT YEAR(GETDATE()))
+SELECT @ANO AS ANO
+
+DECLARE @ANO2 CHAR(4)
+SET @ANO2 = (SELECT (YEAR(GETDATE())))
+SELECT @ANO2 AS ANO2
