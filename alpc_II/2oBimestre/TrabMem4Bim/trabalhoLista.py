66fc33065ccce42311212d6a21449102e3223828
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2022-11-11 20:12:36 -0300

    trabalho feito em lista pronto

--- alpc_II/2oBimestre/TrabMem4Bim/trabalhoLista.py
+++ alpc_II/2oBimestre/TrabMem4Bim/trabalhoLista.py
@@ -8,7 +8,7 @@ def primeira_escolha(tamanho,letra, memoria, testememoria): # PRIMEIRA ESCOLHA
         if memoria[i] == ' ':
             if tamanho == 1:
                 memoria[i] = letra
-                return memoria # FINALIZA O ALGORITIMO PARA TAMANHO 1
+                finalizar(memoria, testememoria) # FINALIZA O ALGORITIMO PARA TAMANHO 1
             else:
                 if i < 99: # VERIFICA SE O TAMANHO É SUFICIENTE
                     if memoria[i+1] == ' ':
@@ -21,10 +21,48 @@ def primeira_escolha(tamanho,letra, memoria, testememoria): # PRIMEIRA ESCOLHA
                     for j in range(tamanho):
                         memoria[listavazios[j]] = letra
                     finalizar(memoria, testememoria)
-                    return memoria # FINALIZA O ALGORITIMO PARA TAMANHO MAIOR QUE 1  
     finalizar(memoria, testememoria)
+    
+def pior_melhor_escolha(tamanho, letra, memoria, testememoria, opcao): # PIOR ESCOLHA
+    listavazios = []
+    asposicoes = []
+    cont = 1
+    for i in range(100):
+        if memoria[i] == ' ':
+            if listavazios == []:
+                listavazios.append(i)   
+            if i < 99:
+                if memoria[i+1] == ' ':
+                    listavazios.append(i+1)
+                else:
+                    if len(listavazios) >= tamanho:
+                        if cont == 1:
+                            asposicoes = listavazios.copy()
+                            cont = 0
+                        if opcao == 3:
+                            if len(asposicoes) < len(listavazios):
+                                asposicoes = listavazios.copy()
+                                listavazios.clear()
+                            else:
+                                listavazios.clear()
 
-
+                        if opcao == 2:
+                            asposicoes = listavazios.copy()
+                            if len(asposicoes) == tamanho:
+                                asposicoes = listavazios.copy()
+                                break
+                            if len(asposicoes) > len(listavazios):
+                                asposicoes = listavazios.copy()
+                                listavazios.clear()
+                            else:
+                                listavazios.clear()
+                    else:
+                        listavazios.clear()
+                        
+    for j in range(tamanho):
+        memoria[asposicoes[j]] = letra
+    finalizar(memoria, testememoria)
+    
 def finalizar(memoria, testememoria): #FINALIZAR ALGORITIMO
     if memoria == testememoria:
         print("\nNão foi possível alocar a memória")
@@ -32,24 +70,13 @@ def finalizar(memoria, testememoria): #FINALIZAR ALGORITIMO
         print("\nMemória alocada com sucesso")
         return memoria    
 
-# INICIALIZAR MEMORIA
-'''
-memoria = [' '] * 100
-opcao = 0
-tamanho = 0
-letra = ''
-for i in range(100):
-    if(random.randint(0, 11) >= 5):
-        memoria[i] = 'x'
-    else:
-        memoria[i] = ' '
-'''
-
 
 memoria =  ['x', 'x', 'x', ' ', 'x', 'x', 'x', 'x', 'x', 'x', ' ', ' ', ' ', ' ', 'x', 'x', 'x', 'x', ' ', ' ', ' ', 'x', ' ', ' ', ' ', 'x', 'x', 'x', 'x', 'x', ' ', ' ', ' ', 'x', ' ', 'x', ' ', ' ', ' ', 'x', 'x', 'x', ' ', ' ', ' ', 'x', ' ', 'x', 'x', ' ', 'x', ' ', ' ', 'x', ' ', 'x', ' ', ' ', 'x', 'x', 'x', ' ', 'x', ' ', ' ', ' ', 'x', 'x', 'x', 'x', 'x', 'x', 'x', ' ', ' ', 'x', ' ', 'x', ' ', 'x', 'x', ' ', ' ', ' ', ' ', ' ', ' ', 'x', ' ', ' ', 'x', ' ', 'x', 'x', ' ', 'x', ' ', 'x', 'x', ' ']
 opcao = 0
 tamanho = 0
 letra = ''	
+
+
 
 def imprimememoria(): # IMPRIMIR MEMORIA
     for i in range(100):
@@ -62,6 +89,8 @@ def entradas(): # ENTRADA DE DADOS
     letra = input("Letra: ")
     return tamanho, letra     
 
+
+imprimememoria()
 while True: # MENU
     print("\n\n1 - Primeira Escolha\n2 - Melhor Escolha\n3 - Pior Escolha\n4 - Imprimir Memória\n5 - Sair")
     opcao = int(input(("\nOq deseja fazer? ")))  # OPÇÃO DO MENU
@@ -76,5 +105,8 @@ while True: # MENU
             if opcao == 1:
                 tamanho, letra = entradas()
                 primeira_escolha(tamanho,letra, memoria, testememoria)
+            if opcao == 3 or opcao == 2:
+                tamanho, letra = entradas()
+                pior_melhor_escolha(tamanho,letra, memoria, testememoria, opcao)
             else:
-                print("Opção inválida")
+                print("Opção inválida")           
