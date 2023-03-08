f04bc4c4014d28982272a3f456b4ec4ed2eb9b05
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-03-08 19:05:47 -0300

    aula2BD

---
+++ bd_II/1oBimestre/aula02.sql
@@ -0,0 +1,33 @@
+USE MinhaCaixa
+
+
+CREATE FUNCTION DtsMinutos
+        (@min int, @dti datetime, @dtf datetime)
+
+RETURNS @tbl TABLE (dt datetime)
+AS
+BEGIN
+    WHILE @dti <= @dtf
+    BEGIN
+        INSERT INTO @tbl(dt) VALUES (@dti)
+        SET @dti = DATEADD(MINUTE, @min,@dti)
+    END
+    RETURN
+END
+
+
+SELECT * FROM DtsMinutos(1,GETDATE(),GETDATE()+1)
+
+
+
+
+CREATE FUNCTION clienteApos(@dt datetime)
+RETURNS TABLE
+AS
+RETURN (SELECT * FROM Clientes WHERE ClienteNascimento >= @dt)
+
+select * from clientes
+    INNER join 
+        clienteApos('1980-01-01') as fn
+            on clientes.ClienteNascimento 
+                    =fn.ClienteNascimento
