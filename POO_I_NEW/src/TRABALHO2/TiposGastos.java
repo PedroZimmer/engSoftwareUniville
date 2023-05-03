0cc9f1b08ad43e8b36a36adb4f5a769479a65044
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-05-03 19:24:11 -0300

    limpando

--- POO_I_NEW/src/TRABALHO2/TiposGastos.java
+++ POO_I_NEW/src/TRABALHO2/TiposGastos.java
@@ -1,25 +1,15 @@
 package TRABALHO2;
 
 public class TiposGastos {
-
     private String TipoGastos;
 
     public TiposGastos(String tipoGastos) {
         TipoGastos = tipoGastos;
     }
 
-    public String getTipoGastos() {
-        return TipoGastos;
-    }
-
-    public void setTipoGastos(String tipoGastos) {
-        TipoGastos = tipoGastos;
-    }
-
     public String toString() {
         return TipoGastos;
     }
-
 
 
 }
