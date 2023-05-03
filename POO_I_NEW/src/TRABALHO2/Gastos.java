968a1e908b792e38d5a788c6bb67439c11e00148
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-05-03 19:27:43 -0300

    limpando

--- POO_I_NEW/src/TRABALHO2/Gastos.java
+++ POO_I_NEW/src/TRABALHO2/Gastos.java
@@ -18,33 +18,18 @@ public class Gastos {
         return DataGastos;
     }
 
-    public void setDataGastos(String dataGastos) {
-        DataGastos = dataGastos;
-    }
-
     public double getValorGastos() {
         return ValorGastos;
-    }
-
-    public void setValorGastos(double valorGastos) {
-        ValorGastos = valorGastos;
     }
 
     public String getTipoGastos() {
         return TipoGastos;
     }
 
-    public void setTipoGastos(String tipoGastos) {
-        TipoGastos = tipoGastos;
-    }
-
     public String getFormaPagamento() {
         return FormaPagamento;
     }
 
-    public void setFormaPagamento(String formaPagamento) {
-        FormaPagamento = formaPagamento;
-    }
 }
 
 
