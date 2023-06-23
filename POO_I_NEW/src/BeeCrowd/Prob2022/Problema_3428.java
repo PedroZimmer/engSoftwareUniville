b6b3ddbffd03276c274308c04c2807218f172af9
Pedro Eccel Zimmermann <pedro.eccel.zimmermann@gmail.com>
2023-06-23 19:49:29 -0300

    tentando

---
+++ POO_I_NEW/src/BeeCrowd/Prob2022/Problema_3428.java
@@ -0,0 +1,65 @@
+package BeeCrowd.Prob2022;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Problema_3428 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+//        HashMap<Integer,Integer> alturas = new HashMap<>();
+        ArrayList<Integer> lista = new ArrayList<>();
+        // <NUMERO DO BALAO, ALTURA DO BALAO>
+
+        System.out.println("qtd baloes:");
+        int numBaloes = sc.nextInt();
+
+        System.out.println("alturas:");
+        for (int i=0; i < numBaloes; i++) {
+            lista.add(sc.nextInt());
+        }
+
+        int x = 0;
+        int flechas = 0;
+        int estourados = 0;
+
+
+        while (estourados < numBaloes) {
+            estourados++;
+            int balao = 0;
+
+//            for (int i = 0; i < lista.size()-1; i++) { //talvez trocar a variavel numBaloes para estourados deixe mais eficiente
+//                if (lista.get( i + 1 ) == lista.get(balao) - 1 ) {
+//                   estourados++;
+//                   lista.remove(balao);
+//                   balao = i; // acho q da pra tirar esse balao e deixar somente i
+//                   i= 0;
+//                }
+//
+//            }
+//            flechas++;
+
+            int i = 0;
+            while ( true) {
+
+                if (lista.get( i + 1 ) == lista.get(balao) - 1 ) {
+                    estourados++;
+                    lista.remove(0);
+//                    balao = i; // acho q da pra tirar esse balao e deixar somente i
+                    i= 0;
+                }
+                i++;
+
+
+            }
+
+
+
+        }
+        System.out.println(flechas);
+
+
+
+    }
+}
